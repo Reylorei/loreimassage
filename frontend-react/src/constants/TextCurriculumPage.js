08333e4c3f6255a5e5bee66088ba2cf1dc83d158
Reylorei <165697412+Reylorei@users.noreply.github.com>
2024-08-30 04:59:03 -0500

    Create TextCurriculumPage.js

---
+++ frontend-react/src/constants/TextCurriculumPage.js
@@ -0,0 +1,61 @@
+
+
+const descriptionsInCurriculumsPage = {
+    es: {
+      initial: "HOLA! Bienvenidx al Centro de Quiromasaje y Bienestar Lorei. Por favor, SELECCIONE la experiencia acorde a sus necesidades:",
+      drawing1: "El Masaje Descontracturante te ayudará con la sobrecarga muscular causada por posturas prolongadas, ejercicio intenso, o durante la fase final de recuperación tras una lesión.",
+      drawing2: "¿Quieres desconectar y dejarte llevar por los sentidos? El Masaje Relajante es perfecto para reducir el estrés diario, producto de la vida laboral o personal.",
+      drawing3: "Prueba el Masaje Circulatorio si buscas quitarte esa sensación de pesadez, así como mejorar la circulación del cuerpo en general.",
+      buttonText: [
+        "ALIVIAR TENSIONES",
+        "DESPEJAR LA MENTE",
+        "RECUPERAR LIGEREZA"
+      ]
+    },
+    en: {
+      initial: "HELLO! Welcome to the Lorei Massage and Wellness Center. Please, SELECT the appropriate experience for your needs:",
+      drawing1: "The Deep Tissue Massage will assist you with muscle tension caused by prolonged postures, intense exercise, or during the final phase of recovery after an injury.",
+      drawing2: "Do you want to disconnect and let yourself be carried away by your senses? The Relaxation Massage is perfect for reducing daily stress, whether from work or personal life.",
+      drawing3: "Experience the Circulatory Massage to effectively alleviate that sensation of heaviness and enhance overall body circulation.",
+      buttonText: [
+        "LOOSEN UP THE BODY",
+        "RELAX THE MIND",
+        "REGAIN LIGHTNESS"
+      ]
+    },
+    it: {
+      initial: "CIAO! Benvenuti al Centro di Massaggio e Benessere Lorei. Per favore, SCEGLI l'esperienza in base alle tue esigenze:",
+      drawing1: "Il Massaggio Decontratturante ti aiuterà con il sovraccarico muscolare causato da posture prolungate, esercizio intenso o durante la fase finale di recupero dopo un'infortunio.",
+      drawing2: "Vuoi staccare la spina e lasciarti trasportare dai sensi? Il Massaggio Rilassante è perfetto per ridurre lo stress quotidiano, dovuto alla vita lavorativa o personale.",
+      drawing3: "Prova il Massaggio Circolatorio se desideri liberarti di quella sensazione di pesantezza, oltre a migliorare la circolazione del corpo in generale.",
+      buttonText: [
+        "DECONTRARRE I MUSCOLI",
+        "RILASSARE LA MENTE",
+        "RECUPERARE LEGGEREZZA"
+      ]
+    },
+    de: {
+      initial: "HALLO! Willkommen im Lorei Massage- und Wellnesscenter. Bitte, WÄHLEN Sie das passende Erlebnis für ihre Bedürfnisse aus:",
+      drawing1: "Die Tiefengewebsmassage wird Ihnen bei Muskelverspannungen helfen, die durch anhaltende Haltungen, intensives Training oder während der Endphase der Genesung nach einer Verletzung verursacht werden.",
+      drawing2: "Möchten Sie abschalten und sich von Ihren Sinnen mitreißen lassen? Die Entspannungsmassage ist perfekt, um den täglichen Stress zu reduzieren, sei es durch Arbeit oder das persönliche Leben.",
+      drawing3: "Erleben Sie die Kreislaufmassage, um diese Schweregefühl effektiv zu lindern und die allgemeine Körperzirkulation zu verbessern.",
+      buttonText: [
+        "DEN KÖRPER LOCKERN",
+        "ENTSPANNE DEN GEIST",
+        "ERNEUTE FREIHEIT"
+      ]
+    },
+    is: {
+      initial: "HÆ! Velkomin á Lorei Heilsustofuna. Vinsamlegast, VELDU þá upplifun sem þú ert að leita að:",
+      drawing1: "Til að létta á vöðvaspennu sem valdið er af miklum æfingum, langvarandi rangri líkamsstöðu eða lokastigi endurhæfingar eftir meiðsli.",
+      drawing2: "Viltu slaka á og aftengja þig frá veruleikanum? Fullkomið til að draga úr daglegri streitu, sem fylgir oft vinnu- eða persónulegu lífi.",
+      drawing3: "Þrýstings og sogæða nudd er frábær leið til að losa þig við þessa þungu tilfinningu í líkamanum og bæta blóðrás líkamans.",
+      buttonText: [
+        "VÖÐVANUDD",
+        "HUGARRÓ",
+        "ENDURHEIMTA VELLIÐAN"
+      ]
+    }
+  };
+
+  export default descriptionsInCurriculumsPage;
